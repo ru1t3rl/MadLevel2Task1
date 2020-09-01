99b1fcb0b6c3fd08303f608d43705c0255bd1ba2
ru1t3rl <luca.ruiters@hva.nl>
2020-09-01 15:42:15 +0100

    Linked the Adapter to the RecyclerView

--- app/src/main/java/tech/tucano/madlevel2task1/MainActivity.kt
+++ app/src/main/java/tech/tucano/madlevel2task1/MainActivity.kt
@@ -2,14 +2,31 @@ package tech.tucano.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import tech.tucano.madlevel2task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews(){
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+        binding.rvPlaces.adapter = placeAdapter
+
+        for(i in Place.PLACE_NAMES.indices){
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RED_DRAWABLES_IDS[i]))
+        }
+
+        placeAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/tech/tucano/madlevel2task1/PlaceAdapter.kt
+++ app/src/main/java/tech/tucano/madlevel2task1/PlaceAdapter.kt
@@ -1,17 +1,37 @@
 package tech.tucano.madlevel2task1
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import tech.tucano.madlevel2task1.databinding.ItemPlaceBinding
 
 class PlaceAdapter (private val places: List<Place>) :
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>(){
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding = ItemPlaceBinding.bind(itemView)
 
-        fun databind(place: Place){
-            binding.placeImg.setImageResource(place.imageResId)
-            binding.placeLbl.text = place.name
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(places[position])
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+        val binding = ItemPlaceBinding.bind(itemView)
+
+
+        fun bind(place : Place) {
+            binding.placeIv.setImageResource(place.imageResId)
+            binding.placeTxt.text = place.name
         }
     }
 }
